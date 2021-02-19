cd7b11ae6564823fd374b8b3d4face08d4fd717c
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-02-19 08:49:13 -0300

    structure layout

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Content from "../components/layout/Content";
+import Menu from "../components/layout/Menu";
 import "./App.css";
 
 const App = props => (
   <div className="App">
-    Component App
+    <Menu></Menu>
+    <Content></Content>
   </div>
 )
 
